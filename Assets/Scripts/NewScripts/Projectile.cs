c7aa51401b86f4c192d734ac616dc26e1f96fbaf
DevDhawan <71658729+DevDhawan@users.noreply.github.com>
2021-01-12 09:15:50 -0800

    Fixes

--- Assets/Scripts/NewScripts/Projectile.cs
+++ Assets/Scripts/NewScripts/Projectile.cs
@@ -1,52 +1,57 @@
-﻿//////////////////
-//By: Dev Dhawan
-//Date: 1/11/2020
-//Description: Projectile Logic for 2D game.
-//////////////////
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    //where the projectile should be fired from
+    //Spawn Point for projectiles
     public Transform firePoint;
-
-    //gameobject used as the main projectile to be fired
+    //projectile
     public GameObject proj1;
     public ProjectileMotion projectile;
-    //counter for the amount of kunai the user has
+    //Projectile variables
     public int kunai = 20;
-    //the max amount of kunai the user can have
     public int addKunai = 20;
+    private int temp;
+    private bool boolKunai;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        boolKunai = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //check if player has enough kunai and has pressed space
-        if (Input.GetKeyDown(KeyCode.Space) && kunai <= 0)
+        //Checks if there are avaialble kunai
+        if (kunai <= 0)
+        {
+            boolKunai = false;
+        }
+        else
+        {
+            boolKunai = true;
+        }
+        //checks when to fire
+        if (Input.GetKeyDown(KeyCode.Space) && boolKunai == true)
         {
             kunai--;
             Fire();
         }
     }
 
-    //spawn the kunai
+    //Spawn kunai location
     void Fire ()
     {
         //projectile logic
         Instantiate(proj1, firePoint.position, firePoint.rotation);
     }
 
-    //function that increases kunai by a specified amount
+    //increase kunai based on event
     public void IncreaseKun(int amount)
     {
-        kunai += amount;
+        temp = kunai + amount;
+        kunai = temp;
     }
 }
