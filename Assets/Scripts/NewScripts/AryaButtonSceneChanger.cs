4edb61193f8d7efd83799ff1aa2615868ccef0f0
Arya Khubcher <arya.khubcher@digipen.edu>
2021-01-13 08:07:12 -0800

    Button Scene Changer script is completed

---
+++ Assets/Scripts/NewScripts/AryaButtonSceneChanger.cs
@@ -0,0 +1,28 @@
+﻿/*******************
+ * Name: Arya Khubcher
+ * Date: 1/12/21
+ * Desc: When this script is put into any button, when the button is pushed, it will change the scene from the current scene to the scene its set to go to next.
+ *////////////////// 
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AryaButtonSceneChanger : MonoBehaviour
+{
+    public string NextLevel;
+
+    public void Play()
+    {
+        //load the application
+        SceneManager.LoadScene(NextLevel);
+    }
+
+    public void Quit()
+    {
+        //quit the application
+        Application.Quit();
+    }
+}
