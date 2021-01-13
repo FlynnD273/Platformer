a25e9f9bde9e5ffef37ee545d30634c3460ee46c
Arya Khubcher <arya.khubcher@digipen.edu>
2021-01-13 11:28:43 -0800

    added the arrow trigger shoot script

---
+++ Assets/Scripts/NewScripts/AryaArrowTriggerShoot.cs
@@ -0,0 +1,22 @@
+﻿/********
+ * Arya Khubcher
+ * 1/13/21
+ * Desc: when this is added to the trigger, when the trigger is activated, then shoot arrows in random directions.
+ *////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AryaArrowTriggerShoot : MonoBehaviour
+{
+    public GameObject arrows;
+    public GameObject player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Instantiate(arrows, player.transform.position, Quaternion.identity);
+        }
+    }
+}
