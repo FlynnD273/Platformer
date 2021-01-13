a3811432e3c8a41f1efd7bcb8f4b3f7e367b8a33
Flynn Duniho <flynn@harveyosity.org>
2021-01-13 10:27:27 -0800

    Merge branch 'master' of https://github.com/FlynnD273/Platformer
    # Conflicts:
    #	Assets/Textures/Player/Renders/Down/Image0076.png.meta
    #	Assets/Textures/Player/Renders/Down/Image0077.png.meta
    #	Assets/Textures/Player/Renders/Down/Image0078.png.meta
    #	Assets/Textures/Player/Renders/Down/Image0079.png.meta
    #	Assets/Textures/Player/Renders/Down/Image0080.png.meta
    #	Assets/Textures/Player/Renders/Fall/0036.png.meta
    #	Assets/Textures/Player/Renders/Fall/0037.png.meta
    #	Assets/Textures/Player/Renders/Fall/0038.png.meta
    #	Assets/Textures/Player/Renders/Fall/0039.png.meta
    #	Assets/Textures/Player/Renders/Fall/Image0081.png.meta

---
+++ Assets/Scripts/NewScripts/ProjectileMotion.cs
@@ -0,0 +1,72 @@
+﻿//////////////////
+//By: Dev Dhawan
+//Date: 1/11/2020
+//Description: The motion of the Projectile.
+//////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMotion : MonoBehaviour
+{
+    //speed and damage vars
+    public float speed = 20f;
+    public int damage = 40;
+
+    //rotation variables
+    public bool boolRotate = false;
+    public float rotationSpeed = -5f;
+
+    //Death Time
+    public float deathTime = 8.0f;
+
+    //declare components
+    public Rigidbody2D myRB;
+    private Projectile projectile;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the projectile speed when the projectile is spawned
+        myRB.velocity = transform.right * speed;
+        projectile = FindObjectOfType<Projectile>();
+    }
+
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        //if the projectile hits an enemy, deal damage to it and despawn projectile
+        Enemy myEnemy = collision.GetComponent<Enemy>();
+        if (myEnemy != null)
+        {
+            myEnemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //destroy kunai if it hits player
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Tilemap"))
+        {
+            //stop the projectile if it hits tiles
+            myRB.constraints = RigidbodyConstraints2D.FreezeAll;
+            boolRotate = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boolRotate == true)
+        {
+            transform.Rotate(0, 0, rotationSpeed);
+        }
+        Destroy(gameObject, deathTime);
+    }
+}
