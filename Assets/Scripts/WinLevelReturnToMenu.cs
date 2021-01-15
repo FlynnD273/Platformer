982aceb0f92e2e8f96515e87ae6643ada7a974f7
cooperspring <71714356+cooperspring@users.noreply.github.com>
2021-01-15 08:06:58 -0800

    Win Level
    The win level is now functional and the button has been resized.

---
+++ Assets/Scripts/WinLevelReturnToMenu.cs
@@ -0,0 +1,13 @@
+﻿//Cooper Spring, 1/15/2021, Simple script for the win level button to return player to main menu
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLevelReturnToMenu : MonoBehaviour
+{
+    //send player to main menu
+    public void SendToMenu()
+    {
+        GameManager.Manager.SetLevel(0);
+    }
+}
