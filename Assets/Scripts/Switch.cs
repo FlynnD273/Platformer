340b3148b6588271e5a6ae76632b725209c62b02
DevDhawan <devdhawan35@gmail.com>
2021-01-20 14:42:11 -0800

    Revert "Revert "update""
    This reverts commit 1319807dfde7364868bc86e7f50596a70670569c.

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,32 @@
+﻿//////////////////
+//By: Dev Dhawan
+//Date: 1/18/2020
+//Description: Switch script.
+//////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject wall;
+    public Color checkActive = new Color(1, 0, 1, 1);
+    public bool doDestroyORCreate = true;
+    //Checks if collision with kunai happens
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("kunai"))
+        {
+            if (doDestroyORCreate == true)
+            {
+                Destroy(wall);
+            }
+            if (doDestroyORCreate == false)
+            {
+                wall.SetActive(true);
+            }
+            gameObject.GetComponent<SpriteRenderer>().color = checkActive;
+            Destroy(collision.gameObject);
+        }
+    }
+}
