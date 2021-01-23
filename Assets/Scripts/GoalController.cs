b98e6ae068d5d5db04d7a60d10861738f9dfa119
Flynn Duniho <flynn@harveyosity.org>
2021-01-22 22:46:56 -0800

    Added Goal

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -21,7 +21,7 @@ public class GoalController : MonoBehaviour
     void Update()
     {
         transform.position = (Vector3)SpawnPoint + new Vector3(0, Mathf.Sin(Time.realtimeSinceStartup * 1.2f + 0.3f) * 0.5f, transform.position.z);
-        sr.color = Color.HSVToRGB(((Time.realtimeSinceStartup / 10) % 1), 1, 1);
+        //sr.color = Color.HSVToRGB(((Time.realtimeSinceStartup / 10) % 1), 1, 1);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
